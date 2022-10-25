5cbe5b1d77d181baa2e532b9478b83bd2a8dcce4
A. Matthias Duarte <im.h1pst@gmail.com>
2022-10-25 01:38:45 -0500

    end course php

--- 17_oop.php
+++ 17_oop.php
@@ -19,23 +19,49 @@ class User {
     public $email;
     public $password;
 
-    // Method is a 
+    // A constructor is a method that runs when an object is created
+    public function __construct( $name, $email, $password) {
+        $this->name = $name;
+        $this->email = $email;
+        $this->password = $password;
+    }
 
+    // Method is a function that belongs to a class
+    function set_name($name) {
+        $this->name = $name;
+    }
 
-
+    function get_name() {
+        return $this->name;
+    }
 }
 
-
-
 // Instatiate a user Object
-$user1 = new User();
-$user2 = new User();
+$user1 = new User('Alan', 'alan@gmail.com', '34654');
+$user2 = new User('Jhon', 'jhon@gmail.com', 'kflg7');
 
-$user1->name = 'Alan Matt';
+// $user1->name = 'Alan Matt';
+
+//echo $user1->email;
+//echo $user2->name;
 
 
-var_dump($user1);
- 
+//Inheritence
+class Employee extends User {
+    public function __construct($name, $email, $password, $title)
+    {
+        parent::__construct($name, $email, $password);
+        $this->title = $title;
+    }
+
+    public function get_title() {
+        return $this->title;
+    }
+}
+
+$employee1 = new Employee('Sara', 'sara@gmail.com', '48521', 'Manager');
+
+echo $employee1->get_title();
 
 
 ?>
